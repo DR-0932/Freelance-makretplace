4197958d2ae03bd1cecfbecfb5c1988111a8b340
Dhruv <dhruvrajput757@gmail.com>
2026-09-03 19:59:16 +0530

    feat(marketing): update marketing landing page

--- apps/frontend/src/app/(marketing)/page.tsx
+++ apps/frontend/src/app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "@/components/marketing/heroSection";
 export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen p-12">
-      {/* <HeroSection /> */}
+      <HeroSection />
       <BentoGridThirdDemo/>
     </main>
   );
